4f216dd83dfab7f7a88505b163e2aa630cc320ec
MatiBruzzoni <104027910+MatiBruzzoni@users.noreply.github.com>
2024-02-27 05:32:05 -0300

    Ejercicio_10

---
+++ Algoritmos_Y_Estructuras_de_datos_2022_UTN_FRSR/Trabajo_Practico_01/Practico_N1_Completo/Ejercicio_10.py
@@ -0,0 +1,9 @@
+#Ejercio10
+x19=int(input("ingrese un numero ="))
+x20=int(input("ingrese otro numero ="))
+if x19==x20:
+        print("los 2 numeros son iguales")
+elif x19 > x20:
+    print(f"el numero {x19} es mayor que {x20}:")
+else:
+    print(f"el numero {x20} es mayor que {x19}:")
