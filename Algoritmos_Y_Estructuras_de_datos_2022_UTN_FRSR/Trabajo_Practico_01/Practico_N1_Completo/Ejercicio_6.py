3b3205da5fe38927376dd5c0eca5aac8e567d03c
MatiBruzzoni <104027910+MatiBruzzoni@users.noreply.github.com>
2024-02-27 05:30:14 -0300

    Ejercicio_6

---
+++ Algoritmos_Y_Estructuras_de_datos_2022_UTN_FRSR/Trabajo_Practico_01/Practico_N1_Completo/Ejercicio_6.py
@@ -0,0 +1,11 @@
+#Ejercicio 6
+x3=int (input("Ingrese un valor numerico ="))
+x4=int (input("Ingrese un segundo valor numerico ="))
+suma = x3 + x4
+resta= x3 - x4
+multiplicacion= x3*x4
+division= x3/x4
+print (suma)
+print (resta)
+print (multiplicacion)
+print (division)
