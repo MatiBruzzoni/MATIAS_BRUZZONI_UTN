5a791123ebbfcf42b18482cc48a34633770177d5
MatiBruzzoni <104027910+MatiBruzzoni@users.noreply.github.com>
2024-02-27 05:32:35 -0300

    Ejercicio_11

---
+++ Algoritmos_Y_Estructuras_de_datos_2022_UTN_FRSR/Trabajo_Practico_01/Practico_N1_Completo/Ejercicio_11.py
@@ -0,0 +1,5 @@
+dolar=float(input("Ingrese el monto de la transaccion en dolares: "))
+pesos= 200*dolar
+print("La cotizacion del dolar el dia de hoy es de 200 Pesos por Dolar")
+print(f"El valor de la transaccion en pesos es {pesos}")
+print(f"El total de la transaccion con la comision del banco es {pesos*1.04}")
